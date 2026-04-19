a11e767d3bf209d5f545a8a392864f4e64140fd5
tim <a0927555643@gmail.com>
2026-04-19 16:04:51 +0800

    修正為os.environ用法

--- pm25.py
+++ pm25.py
@@ -40,12 +40,13 @@ def insert_data(data):
 
 def open_db():
     try:
+        #print(os.getenv("HOST")) //os.getenv給本地端dotenv使用
         conn=pymysql.connect(
-            host=os.getenv("HOST"),
-            port=int(os.getenv("PORT")),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            database=os.getenv("NAME"),
+            host=os.environ.get("HOST"),
+            port=int(os.environ.get("PORT")),
+            user=os.environ.get("USER"),
+            password=os.environ.get("PASSWORD"),
+            database=os.environ.get("NAME"),
             ssl={"ca":None}
         )
 
